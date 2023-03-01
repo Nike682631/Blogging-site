ec3c306f1d4869d8d6407f25515bdc3ce5188e07
nikunj <nikunjgoyal31@gmail.com>
2023-03-01 16:09:17 +0530

    added input fields for login and some other stuff

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,49 +1,55 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+    <div class="row">
+      <div class="col-12 col-lg-4 q-pa-md">
+        <div class="text-h4 text-bold">Blogs</div>
+        <div class="text-grey-7">Please login with us</div>
+        <div class="column">
+          <q-input class="q-mt-sm" label="Enter email" outlined v-model="form.email.value" :error="form.email.error"
+            :error-message="form.email.msg">
+            <template v-slot:prepend>
+              <q-icon name="email" />
+            </template>
+          </q-input>
+          <q-input class="q-my-sm" label="Enter password" outlined v-model="form.password.value"
+            :error="form.password.error" :error-message="form.password.msg">
+            <template v-slot:prepend>
+              <q-icon name="lock" />
+            </template>
+            <template v-slot:append>
+              <q-icon name="visibility_off" />
+            </template>
+          </q-input>
+        </div>
+        <div class="column">
+          <q-btn color="indigo" @click="submitLogin">
+            Login
+          </q-btn>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'IndexPage',
-  components: { ExampleComponent },
-  setup () {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
+  components: {},
+  setup() {
+    const form = ref({
+      email: {
+        value: '',
+        required: true
       },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
+      password: {
+        value: '',
+        required: true
       }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
-    return { todos, meta };
+    })
+    const submitLogin = () => { return; }
+    return { form, submitLogin };
   }
 });
 </script>
